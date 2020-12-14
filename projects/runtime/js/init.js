9c633f903e18e1e637148f2e58de3dbf6a21dcdd
rileyzzz2 <rlemmler@icloud.com>
2020-12-14 14:15:53 +0000

    phil

--- projects/runtime/js/init.js
+++ projects/runtime/js/init.js
@@ -33,6 +33,9 @@ var init = function (window) {
 
     // TODO 2 : add background
 
+    var background = opspark.makeBackground(app,ground);
+    view.addChild(background);
+
     
     var help = draw.textfield('MOVES || up: jump | right: flying jump | down: duck | space: fire | q self destruct!', 
         '20px Arial',
@@ -71,7 +74,9 @@ var init = function (window) {
     }
     
     // TODO 1 : add a heads-up display to game
-
+    var hud = opspark.makeHud();
+    view.addChild(hud);
+    window.hud = hud;
 
     // TODO 6 : create game manager
 
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -36,11 +36,17 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,groundY,'black');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
-            
+            var phil = draw.bitmap('img/phil.png');
+            phil.x = 300;
+            phil.y = 25;
+            phil.scaleX = 1.0;
+            phil.scaleY = 1.0;
+            background.addChild(phil);
+
             
             // TODO: 5 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
