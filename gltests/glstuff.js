728a6aea377b455978eda70d95574af798b66ebb
rileyzzz2 <rlemmler@icloud.com>
2020-12-14 14:01:21 +0000

    ok how about now

---
+++ gltests/glstuff.js
@@ -0,0 +1,54 @@
+var vertexShaderSource = `#version 300 es
+ 
+// an attribute is an input (in) to a vertex shader.
+// It will receive data from a buffer
+in vec4 a_position;
+ 
+// all shaders have a main function
+void main() {
+ 
+  // gl_Position is a special variable a vertex shader
+  // is responsible for setting
+  gl_Position = a_position;
+}
+`;
+ 
+var fragmentShaderSource = `#version 300 es
+ 
+// fragment shaders don't have a default precision so we need
+// to pick one. highp is a good default. It means "high precision"
+precision highp float;
+ 
+// we need to declare an output for the fragment shader
+out vec4 outColor;
+ 
+void main() {
+  // Just set the output to a constant reddish-purple
+  outColor = vec4(1, 0, 0.5, 1);
+}
+`;
+
+var canvas;
+var gl;
+$( document ).ready(function() {
+    canvas = document.querySelector("#epicGL");
+    gl = canvas.getContext("webgl");
+    if (gl === null) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  window.requestAnimationFrame(frame);
+});
+
+function frame() {
+    //gl.clearColor(1.0, 0.0, 0.0, 1.0);
+    
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    window.requestAnimationFrame(frame);
+}
