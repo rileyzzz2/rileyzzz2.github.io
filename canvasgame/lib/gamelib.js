444d791b5c99219039507c2d0626910793869b24
rileyzzz2 <rlemmler@icloud.com>
2020-12-17 14:19:08 +0000

    bruh

--- canvasgame/lib/gamelib.js
+++ canvasgame/lib/gamelib.js
@@ -1,3 +1,27 @@
+
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Composite = Matter.Composite,
+    Bodies = Matter.Bodies;
+
+// create an engine
+var engine = Engine.create();
+
+// create two boxes and a ground
+//var boxA = Bodies.rectangle(400, 200, 80, 80);
+//var boxB = Bodies.rectangle(450, 50, 80, 80);
+//var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+//World.add(engine.world, [ground]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+//Render.run(render);
+
 class game {
     //#canvas;
     //#ctx;
@@ -8,37 +32,54 @@ class game {
 
     init() {
         this.canvas = $("#gameWindow")[0];
+        this.height = this.canvas.height;
         //this.ctx = $("#gameWindow")[0].getContext('2d');
         
+        // var render = Render.create({
+        //     canvas: this.canvas,
+        //     engine: engine
+        // });
+        
+        // Render.run(render);
+
         //begin render
         window.requestAnimationFrame((t)=>this.render(t));
     }
 
-    box(isStatic, _x, _y, _w, _h) {
-        this.objects.push({
-            type: "box",
-            static: isStatic,
-            x: _x,
-            y: _y,
-            w: _w,
-            h: _h
-        });
+    box(_x, _y, _w, _h, is_static) {
+        var box = Bodies.rectangle(_x, _y, _w, _h, { isStatic: is_static });
+        this.objects.push(box);
+        World.add(engine.world, [box]);
+        return box;
     }
     render(time) {
         //console.log(time);
         var ctx = this.canvas.getContext('2d');
+        ctx.fillStyle = '#fff';
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         
-        for(let i = 0; i < this.objects.length; i++) {
-            let obj = this.objects[i];
-            switch(obj.type) {
-                case "box":
-                    
-                    break;
-            }
+        var bodies = Composite.allBodies(engine.world);
+        //ctx.fillStyle = 'orange';
+        
+        ctx.beginPath();
+
+        for (var i = 0; i < bodies.length; i++) { 
+            // var r = Math.random() * 255;
+            // var g = Math.random() * 255;
+            // var b = Math.random() * 255;
+            // ctx.fillStyle = 'rgb(' + String(r) + ', ' + String(g) + ', ' + String(b) + ')';
+            var vertices = bodies[i].vertices;
+
+            ctx.moveTo(vertices[0].x, vertices[0].y);
+            for (var j = 1; j < vertices.length; j++)
+                ctx.lineTo(vertices[j].x, vertices[j].y);
+            
+            ctx.lineTo(vertices[0].x, vertices[0].y);
+            ctx.fill();
         }
         
         window.requestAnimationFrame((t)=>this.render(t));
+        
     }
 }
 
--- canvasgame/mygame.js
+++ canvasgame/mygame.js
@@ -1,7 +1,17 @@
 //import g from './lib/gamelib.js';
 
 $( document ).ready(function() {
-    g.box(true, 50, 50, 50, 50);
     g.init();
+
+    //for(let i = 0; i < 20; i++)
+        //g.box(50 + i * 4, 50, 10, 10, false);
+    g.box(0, g.height, 810, 20, true);
+});
+
+$("#gameWindow").click(function() {
+    var posX = $(this).position().left,
+        posY = $(this).position().top;
+    
+    alert((e.pageX - posX) + ' , ' + (e.pageY - posY));
 });
 //alert("test");
