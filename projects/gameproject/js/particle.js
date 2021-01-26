dbac872a27a7e69f63ff0164bf8174ce2a7f30f3
rileyzzz2 <rlemmler@icloud.com>
2021-01-26 20:30:21 +0000

    bruh 6

--- projects/gameproject/js/particle.js
+++ projects/gameproject/js/particle.js
@@ -1,14 +1,29 @@
 //https://aerotwist.com/tutorials/creating-particles-with-three-js/
 //https://github.com/rollup/three-jsnext/blob/master/examples/webgl_points_billboards.html
 class particle {
-    constructor(point, velocity) {
-        this.velocity = velocity;
+    constructor() {
+        this.respawn();
+    }
+    respawn() {
+        this.position.set(0.0, 0.0, 0.0);
+        this.velocity = new THREE.Vector3(
+                        0,
+                        Math.random() * 10.0,
+                        0);
+    }
+    update(deltaTime) {
+        //respawn
+        if (this.position.y < -200)
+            this.respawn();
+        
+        particle.velocity.y -= 0.1; //gravity
+        particle.position.addSelf(this.velocity);
     }
 };
 class sparkParticleSystem {
     constructor() {
         this.particleCount = 1800;
-        this.particles = new THREE.Geometry();
+        this.particleGeom = new THREE.Geometry();
         this.particleAttr = [];
         
         this.particleMaterial = new THREE.PointsMaterial({
@@ -20,23 +35,16 @@ class sparkParticleSystem {
         });
 
         for (var p = 0; p < particleCount; p++) {
-            var pX = Math.random() * 500 - 250,
-                pY = Math.random() * 500 - 250,
-                pZ = Math.random() * 500 - 250,
-                particle = new THREE.Vector3(pX, pY, pZ);
-            particle.velocity = new THREE.Vector3(
-                0,
-                -Math.random(),
-                0); 
-            
-            this.particles.vertices.push(particle);
+            let curParticle = new particle();
+            this.particleAttr.push(curParticle);
+            this.particleGeom.vertices.push(curParticle.position);
         }
 
         this.particleSystem = new THREE.Points(
-            this.particles,
+            this.particleGeom,
             this.pMaterial);
 
-        this.particleSystem.sortParticles = true;
+        //this.particleSystem.sortParticles = true;
         //scene.addChild(this.particleSystem);
         objects.push(this);
     }
@@ -45,15 +53,16 @@ class sparkParticleSystem {
         this.particleSystem.rotation.y += 0.01;
         var pCount = this.particleCount;
         while (pCount--) {
-            let particle = this.particles.vertices[pCount];
-            //respawn
-            if (particle.position.y < -200) {
-                particle.position.y = 200;
-                particle.velocity.y = 0;
-            }
-            particle.velocity.y -= 0.1; //gravity
+            this.particleAttr[pCount].update(deltaTime);
+            // let particle = this.particleGeom.vertices[pCount];
+            // //respawn
+            // if (particle.position.y < -200) {
+            //     particle.position.y = 200;
+            //     particle.velocity.y = 0;
+            // }
+            // particle.velocity.y -= 0.1; //gravity
 
-            particle.position.addSelf(particle.velocity);
+            // particle.position.addSelf(particle.velocity);
         }
 
         this.particleSystem.geometry.__dirtyVertices = true;
